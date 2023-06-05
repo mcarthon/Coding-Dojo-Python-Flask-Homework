ed68801b0b6be38ce288f9cb96a56d38a0f68d7c
Mark Carthon <macarthoniii@gmail.com>
2023-06-05 12:25:25 -0400

    initial commit

---
+++ fundamentals/introducion/file.py
@@ -0,0 +1,85 @@
+num1 = 42
+num2 = 2.3
+boolean = True
+string = 'Hello World'
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
+fruit = ('blueberry', 'strawberry', 'banana')
+print(type(fruit))
+print(pizza_toppings[1])
+pizza_toppings.append('Mushrooms')
+print(person['name'])
+person['name'] = 'George'
+person['eye_color'] = 'blue'
+print(fruit[2])
+
+if num1 > 45:
+    print("It's greater")
+else:
+    print("It's lower")
+
+if len(string) < 5:
+    print("It's a short word!")
+elif len(string) > 15:
+    print("It's a long word!")
+else:
+    print("Just right!")
+
+for x in range(5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+while(x < 5):
+    print(x)
+    x += 1
+
+pizza_toppings.pop()
+pizza_toppings.pop(1)
+
+print(person)
+person.pop('eye_color')
+print(person)
+
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':
+        break
+
+#these are functions
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section
+"""
+
+# print(num3)
+# num3 = 72
+# fruit[0] = 'cranberry'
+# print(person['favorite_team'])
+# print(pizza_toppings[7])
+#   print(boolean)
+# fruit.append('raspberry')
+# fruit.pop(1)
---
+++ fundamentals/introducion/hello_world.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+your_name = "your name"
+
+print("Hello", your_name)
+
+print("Hello " + your_name)
+
+fave_num = 8
+
+print("Hello", fave_num)
+
+print("Hello " + str(fave_num))
+
+food_one = "food one"
+
+food_two = "food two"
+
+strings = "I love {food_one} and {food_two}"
+
+print(strings.format(food_one = food_one, food_two = food_two))
+
+print(f"I love to eat {food_one} and {food_two}")
