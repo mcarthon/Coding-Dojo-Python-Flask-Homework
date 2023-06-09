066f8932ea62d9a40cf745704e41f3acc705b7ed
Mark Carthon <macarthoniii@gmail.com>
2023-06-08 22:04:48 -0400

    went from go fish to 4D chess with the checkerboard

---
+++ flask/fundamentals/Checkerboard/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/<int:url_x>")
+@app.route("/<int:url_x>/<int:url_y>")
+@app.route("/<string:url_color1>")
+@app.route("/<int:url_x>/<string:url_color1>")
+@app.route("/<int:url_x>/<int:url_y>/<string:url_color1>/<string:url_color2>")
+def home_page(url_x: int = 8, url_y: int = 8, url_color1: str = "red", url_color2: str = "black") -> None:
+    return render_template("red-black-board.html", html_color1=url_color1,
+                           html_color2=url_color2,
+                           html_x=url_x,
+                           html_y=url_y)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
