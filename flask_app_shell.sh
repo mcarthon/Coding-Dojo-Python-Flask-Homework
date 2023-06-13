21c86b6c7149761479859a2eea065f10fe7fed87
Mark Carthon <macarthoniii@gmail.com>
2023-06-13 15:28:49 -0400

    added a shell script to automate creating my flask_app directory structure

---
+++ flask_app_shell.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+project_folder_name="users_crud"
+
+mkdir "${project_folder_name}"
+
+cd "${project_folder_name}"
+
+project_name="users"
+
+mkdir flask_app
+
+touch server.py
+
+pipenv install flask PyMySQL
+
+cd flask_app
+
+mkdir config controllers models static templates
+
+touch __init__.py credentials.py .gitignore
+
+touch ./config/mysqlconnection.py 
+
+touch "./controllers/${project_name}.py"
+
+touch "./models/${project_name}.py"
+
+touch "./static/${project_name}.css"
+
+touch "./templates/${project_name}.html"
