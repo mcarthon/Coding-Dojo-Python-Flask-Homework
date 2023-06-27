3f6bbdceaa8c5a4e1e323a02eb1e444501d3ff08
Mark Carthon <macarthoniii@gmail.com>
2023-06-27 15:11:02 -0400

    removed unnecessary code from ajax username form

--- ajax/ajax_api/form_data/flask_app/static/js/site.js
+++ ajax/ajax_api/form_data/flask_app/static/js/site.js
@@ -1,20 +1,10 @@
 var myForm = document.querySelector("form");
 
-
     myForm.onsubmit = function(e) {                  
     
     e.preventDefault();
     
     var form = new FormData(myForm);
-    
-    console.log(document.querySelector("#username").value);
-    console.log(document.querySelector("#email").value);
-    
-    var username = document.querySelector("#username").value;
-    console.log(username);
-    
-    var email = document.querySelector("#email").value;
-    console.log(email);
     
     fetch("http://localhost:5000/create/user", { method : "POST", body : form })
         .then( response => response.json() )
@@ -22,12 +12,6 @@ var myForm = document.querySelector("form");
     
     myForm.reset()
         
-}
-
-function seeInputLive(element) {
-    
-    console.log(element.value);
-    
 }
 
 function getUsers(){
@@ -50,3 +34,5 @@ function getUsers(){
         })
 
 }
+
+getUsers();
